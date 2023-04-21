c42a340a1464bba63fa9bd4ef2f7e677d201cdd2
m-joshua <joshua@intelimina.com>
2023-04-21 21:10:19 +0800

    Render 401 page if current_user is unauthorized

--- lib/middleware/user_authorizer.rb
+++ lib/middleware/user_authorizer.rb
@@ -4,9 +4,22 @@ class UserAuthorizer
   end
 
   def call(env)
-    request = Rack::Request.new env
-    p request.env['warden'].user # current_user
+    @request = Rack::Request.new env
+    return unauthorized if !current_user.is_admin?
 
     @app.call(env)
+  end
+
+  private
+
+  def current_user
+    @request.env['warden'].user # current_user
+  end
+
+  def unauthorized
+    status_code = Rack::Utils::SYMBOL_TO_STATUS_CODE[:unauthorized]
+    html = ActionView::Base.empty.render(file: 'public/401.html')
+
+    [status_code, {'Content-Type' => 'text/html'}, [html]]
   end
 end
