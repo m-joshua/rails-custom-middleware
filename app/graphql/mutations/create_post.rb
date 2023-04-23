1f12d988c23fa9e7b771bb840ef0a23aa2ec6023
m-joshua <joshua@intelimina.com>
2023-04-23 15:02:07 +0800

    Created sample mutation to create post

---
+++ app/graphql/mutations/create_post.rb
@@ -0,0 +1,23 @@
+module Mutations
+  class CreatePost < BaseMutation
+    field :post, Types::PostType, null: false
+    field :errors, [String], null: false
+    field :success, Boolean, null: false
+
+    argument :user_id, Integer, required: true
+    argument :title, String, required: true
+    argument :content, String, required: false
+
+    def resolve(user_id:, title:, content:)
+      post = Post.create(user_id: user_id,
+                         title: title,
+                         content: content)
+
+      return {
+        post: post,
+        errors: post.errors.full_messages,
+        success: post.persisted?
+      }
+    end
+  end
+end
